691366b74ee6a3048e319a29dee9b7ad9cd11436
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-10-18 17:14:45 +0700

    Create base + index + post-detali.html

--- MyBlog/blog/admin.py
+++ MyBlog/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
