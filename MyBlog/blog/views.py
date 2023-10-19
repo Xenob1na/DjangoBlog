1cb1ccb7485047cd1629f59503178f8a9d619b66
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-10-19 16:24:04 +0700

    Pagination

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -5,7 +5,9 @@ from .models import *
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    paginate_by = 3
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
